12583a6d4ba89b9e0fb2a2ce0b7d332c17304659
Ming-Chiang Chang <mc2663@cornell.edu>
2022-08-06 20:04:03 -0400

    Bug fix

--- startup.jl
+++ startup.jl
@@ -1,6 +1,7 @@
 using CrystalShift
 using CrystalShift: Lorentz, PseudoVoigt, FixedPseudoVoigt, Wildcard
 using CrystalShift: BackgroundModel, Gauss, get_free_params, full_optimize!, PeakModCP
+using CrystalShift: FixedBackground
 using CrystalTree
 using CrystalTree: Lazytree, search!, search_k2n!
 using CovarianceFunctions: EQ
