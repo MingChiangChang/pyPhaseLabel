76ff31989a0e5c379ee83dc6193400a55b22f8fc
Ming-Chiang Chang <mc2663@cornell.edu>
2022-08-31 14:33:34 -0400

    Add function for getting probabilities from search results

--- startup.jl
+++ startup.jl
@@ -3,5 +3,5 @@ using CrystalShift: Lorentz, PseudoVoigt, FixedPseudoVoigt, Wildcard
 using CrystalShift: BackgroundModel, Gauss, get_free_params, full_optimize!, PeakModCP
 using CrystalShift: FixedBackground
 using CrystalTree
-using CrystalTree: Lazytree, search!, search_k2n!
+using CrystalTree: Lazytree, search!, search_k2n!, approximate_negative_log_evidence, get_probabilities
 using CovarianceFunctions: EQ
--- test/test_tree.py
+++ test/test_tree.py
@@ -3,13 +3,13 @@ import matplotlib.pyplot as plt
 
 from pyPhaseLabel import PhaseModel, CrystalPhase, EQ, BackgroundModel, FixedPseudoVoigt
 from pyPhaseLabel import create_phases, evaluate_obj, optimize_phase, Lorentz, PseudoVoigt
-from julia.Main import Wildcard, Lazytree, search
+from julia.Main import Wildcard, Lazytree, search, get_probabilities
 
 std_noise = .01
 mean_θ = [1., 1., .2]
 std_θ = [.5, .5, 1.]
 
-with open('sticks.csv', 'r') as f:
+with open('../sticks.csv', 'r') as f:
     t = f.read()
 tt = t.split("#\n")
 tt.remove("")
@@ -17,18 +17,20 @@ tt.remove("")
 phases = create_phases(t, .5, FixedPseudoVoigt(0.1))
 x = np.linspace(15, 55, 1024)
 
-tree = Lazytree(phases, 3, x, 5, tt, False)
+tree = Lazytree(phases[:4], 3, x, 5, tt, False)
 
 y = evaluate_obj(phases[1:4], x)
-plt.plot(x, y)
-plt.show()
+#plt.plot(x, y)
+#plt.show()
 
 result = search(tree, x, y, 3, std_noise, mean_θ, std_θ, maxiter=128, regularization=True)
 
-for idx, node in enumerate(result):
-    a = [np.linalg.norm(evaluate_obj(n.phase_model, x)-y) for n in node]
-    ind = np.argmin(a)
-    plt.plot(x, evaluate_obj(node[ind].phase_model, x))
+results = [r  for subresults in result for r in subresults]
+t = get_probabilities(results, x, y, std_noise, mean_θ, std_θ) 
+for idx, node in enumerate(results):
+    # a = [np.linalg.norm(evaluate_obj(n.phase_model, x)-y) for n in node]
+    # ind = np.argmin(a)
+    plt.plot(x, evaluate_obj(node.phase_model, x))
     plt.plot(x, y)
-    plt.title(f"{idx} phase")
+    plt.title(f"Probability {t[idx]}")
     plt.show()
