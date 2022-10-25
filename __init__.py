175be9e4e7b84f7b499f3569e735e2f1b797517f
Ming-Chiang Chang <mc2663@cornell.edu>
2022-10-25 15:56:10 -0400

    Add line to start julia with nthreads=cpu_core_count

--- __init__.py
+++ __init__.py
@@ -1,3 +1,8 @@
+import os
+from multiprocessing import cpu_count
+os.environ["JULIA_NUM_THREADS"] = str(cpu_count())
+
+
 from .wrapper import evaluate_obj, optimize_phase, create_phases, fit_amorphous, optimize_all
 
 from julia.Main import CrystalPhase, PhaseModel, BackgroundModel, EQ, Wildcard # Important structs 
