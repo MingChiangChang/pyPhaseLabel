262af9e536d4c2e08a4f04d16780855e407f7dcc
Ming-Chiang Chang <mc2663@cornell.edu>
2023-04-23 22:52:22 -0400

    Add optimization mode into function argument

--- python_mod.jl
+++ python_mod.jl
@@ -11,13 +11,16 @@ function optimize(phases::AbstractVector{CrystalPhase}, x::AbstractVector, y::Ab
                    std_noise::Real, mean_θ::AbstractVector = [1., 1., .2],
                    std_θ::AbstractVector = [1., Inf, 5.];
                    method::String="LM", objective::String = "LS",
+		   optimize_mode::String="Simple",
                    maxiter::Int = 32,
                    regularization::Bool = true,
                    verbose::Bool = false, tol::Float64 =DEFAULT_TOL)
     method_enum = get_method_enum(method) 
+    optimize_mode_enum = get_optimize_mode_enum(optimize_mode)
     pm = PhaseModel(phases)
     pm = optimize!(pm, x, y, std_noise, mean_θ, std_θ, method=method_enum,
-                objective=objective, maxiter=maxiter, regularization=regularization,
+                objective=objective, optimize_mode=optimization_mode_enum,
+		maxiter=maxiter, regularization=regularization,
                 verbose=verbose, tol=tol)
     return pm.CPs
 end
@@ -27,14 +30,17 @@ function optimize_with_uncertainty(phases::Any,
                    std_noise::Real, mean_θ::AbstractVector = [1., 1., .2],
                    std_θ::AbstractVector = [1., Inf, 5.];
                    method::String="LM", objective::String = "LS",
+		   optimize_mode::String="Simple",
                    maxiter::Int = 32,
                    regularization::Bool = true,
                    verbose::Bool = false, tol::Float64 =DEFAULT_TOL)
     method_enum = get_method_enum(method)
+    optimize_mode_enum = get_optimize_mode_enum(optimize_mode)
     pm, uncer = optimize_with_uncertainty!(phases, x, y,
 				    std_noise, mean_θ, std_θ,
 				    method=method_enum,
                                     objective=objective,
+				    optimize_mode=optimize_mode_num,
 				    maxiter=maxiter,
 				    regularization=regularization,
                                     verbose=verbose,
@@ -46,6 +52,7 @@ function full_optimize(phases, x::AbstractVector, y::AbstractVector,
                    std_noise::Real, mean_θ::AbstractVector = [1., 1., .2],
                    std_θ::AbstractVector = [1., Inf, 5.];
                    method::String="LM", objective::String = "LS",
+		   optimize_mode::String="Simple",
                    regularization::Bool = true,
                    loop_num::Int=8,
                    peak_shift_iter::Int = 32,
@@ -55,8 +62,10 @@ function full_optimize(phases, x::AbstractVector, y::AbstractVector,
                    peak_mod_iter::Int=32,
                    verbose::Bool = false, tol::Float64 =DEFAULT_TOL)
     method_enum = get_method_enum(method)
+    optimize_mode_enum = get_optimize_mode_enum(optimize_mode)
     pm = full_optimize!(phases, x, y, std_noise, mean_θ, std_θ, method=method_enum,
                 objective=objective, regularization=regularization,
+		optimize_mode=optimize_mode_enum,
 		loop_num = loop_num,
                 peak_shift_iter = peak_shift_iter,
                 mod_peak_num = mod_peak_num,
@@ -70,9 +79,20 @@ end
 
 function get_method_enum(method_str::String)
     if method_str == "LM"
-	    return LM
+	return LM
     elseif method_str =="Newton"
-	    return Newton
+        return Newton
+    end
+end
+
+# TODO: Should be able to generate this with metaprogramming
+function get_optimize_mode_enum(optimize_mode_str::String)
+    if optimize_mode_str == "Simple"
+        return Simple
+    elseif optimize_mode_str == "EM"
+        return EM
+    elseif optimize_mode_str == "WithUncer"
+        return WithUncer
     end
 end
 
@@ -80,12 +100,16 @@ function optimize(pm::PhaseModel, x::AbstractVector, y::AbstractVector,
                 std_noise::Real, mean_θ::AbstractVector = [1., 1., .2],
                 std_θ::AbstractVector = [1., Inf, 5.];
                 method::String = "LM", objective::String = "LS",
+		optimize_mode::String = "Simple",
                 maxiter::Int = 32,
                 regularization::Bool = true,
                 verbose::Bool = false, tol::Float64=DEFAULT_TOL)
     method_enum = get_method_enum(method)
+    optimize_mode_enum = get_optimize_mode_enum(optimize_mode)
     return optimize!(pm, x, y, std_noise, mean_θ, std_θ,
-                method=method_enum, objective=objective, maxiter=maxiter,
+                method=method_enum, objective=objective,
+		optimize_mode=optimize_mode_num,
+		maxiter=maxiter,
                 regularization=regularization, verbose=verbose,
                 tol=tol)
 end
@@ -111,14 +135,36 @@ function evaluate_residual(PM::PhaseModel, x::AbstractVector, r::AbstractVector)
     evaluate_residual!(PM, x, r)
 end
 
-function search(LT::Lazytree, x::AbstractVector, y::AbstractVector, k::Int,
-                 std_noise::Real, mean::AbstractVector, std::AbstractVector;
-                maxiter = 32, regularization::Bool = true, tol::Real = DEFAULT_TOL)
-    search!(LT, x, y, k, std_noise, mean, std, maxiter=maxiter, regularization=regularization, tol=tol)
+const search_k2n = search_k2n!
+function search(LT::Lazytree, x::AbstractVector, y::AbstractVector,
+                 depth::Integer=3, k::Integer=3,
+		 normalization_constant::Real=.1,
+		 amorphous::Bool=false,
+		 background::Bool=false,
+		 background_length::Real=8.,
+		 std_noise::Real=0.05, mean::AbstractVector=[1., .5, .2],
+		 std::AbstractVector=[0.05, 0.05, 0.05];
+                 method::String = "LM", objective::String = "LS",
+                 optimize_mode::String = "Simple", em_loop_num::Integer =8,
+                 maxiter::Integer = 32, regularization::Bool = true,
+		 verbose::Bool = false, tol::Real = DEFAULT_TOL)
+    method_enum = get_method_enum(method)
+    optimize_mode_enum = get_optimize_mode_enum(optimize_mode)
+    search!(LT, x, y, depth, k, normalization_constant, amorphous,
+            background, background_length,
+            std_noise, mean, std,
+            method=method_enum, objective=objective, optimize_mode=optimize_mode_enum,
+            em_loop_num=em_loop_num, maxiter=maxiter, regularization=regularization,
+            verbose=verbose, tol=tol)
 end
+#function search(LT::Lazytree, x::AbstractVector, y::AbstractVector, k::Int,
+#                 std_noise::Real, mean::AbstractVector, std::AbstractVector;
+#                maxiter = 32, regularization::Bool = true, tol::Real = DEFAULT_TOL)
+#    search!(LT, x, y, k, std_noise, mean, std, maxiter=maxiter, regularization=regularization, tol=tol)
+#end
 
-function search_k2n(LT::Lazytree, x::AbstractVector, y::AbstractVector, k::Int,
-                    std_noise::Real, mean::AbstractVector, std::AbstractVector;
-                maxiter = 32, regularization::Bool = true, tol::Real = DEFAULT_TO)
-    search_k2n!(LT, x, y, k, std_noise, mean, std, maxiter=maxiter, regularization=regularization, tol=tol)
-end
+#function search_k2n(LT::Lazytree, x::AbstractVector, y::AbstractVector, k::Int,
+#                    std_noise::Real, mean::AbstractVector, std::AbstractVector;
+#                maxiter = 32, regularization::Bool = true, tol::Real = DEFAULT_TO)
+#    search_k2n!(LT, x, y, k, std_noise, mean, std, maxiter=maxiter, regularization=regularization, tol=tol)
+#end
--- test/test_tree.py
+++ test/test_tree.py
@@ -17,22 +17,25 @@ tt.remove("")
 phases = create_phases(t, .5, FixedPseudoVoigt(0.1))
 x = np.linspace(15, 55, 1024)
 
-tree = Lazytree(phases[:4], 3, x, 5, tt, False)
+tree = Lazytree(phases[:4], x)
 
 y = evaluate_obj(phases[1:4], x)
 #plt.plot(x, y)
 #plt.show()
 
-result = search(tree, x, y, 3, std_noise, mean_θ, std_θ, maxiter=128, regularization=True)
+result = search(tree, x, y, 3, 3, .1, False, False, 0.5,
+                std_noise, mean_θ, std_θ,
+                maxiter=128, regularization=True)
 
-results = [r  for subresults in result for r in subresults]
+results = [r for subresults in result[1:] for r in subresults]
 t = get_probabilities(results, x, y, std_noise, mean_θ, std_θ) 
 ind = np.argmax(t)
 print(get_fraction(results[ind].phase_model.CPs))
-# for idx, node in enumerate(results):
-#     # a = [np.linalg.norm(evaluate_obj(n.phase_model, x)-y) for n in node]
-#     # ind = np.argmin(a)
-#     plt.plot(x, evaluate_obj(node.phase_model, x))
-#     plt.plot(x, y)
-#     plt.title(f"Probability {t[idx]}")
-#     plt.show()
+
+#for idx, node in enumerate(results):
+#    # a = [np.linalg.norm(evaluate_obj(n.phase_model, x)-y) for n in node]
+#    # ind = np.argmin(a)
+#    plt.plot(x, evaluate_obj(node.phase_model, x))
+#    plt.plot(x, y)
+#    plt.title(f"Probability {t[idx]}")
+#    plt.show()
