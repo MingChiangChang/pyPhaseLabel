7d8f51c5a0b862d24c5d677c1f7749d1f4510dcd
Ming-Chiang Chang <mc2663@cornell.edu>
2023-06-15 16:10:30 -0400

    Add installation for BackgroundSubtraction package into

--- install.py
+++ install.py
@@ -11,3 +11,4 @@ from julia import Pkg
 Pkg.add(Pkg.PackageSpec(url = "https://github.com/MingChiangChang/CrystalShift.jl"))
 Pkg.add(Pkg.PackageSpec(url = "https://github.com/MingChiangChang/CrystalTree.jl"))
 Pkg.add("CovarianceFunctions")
+Pkg.add("BackgroundSubtraction")
